fddf09190ff8b7b448cc342122318a87286be5d8
barbaradecares <barbaradecares@hotmail.com>
2019-01-12 17:53:01 -0600

    basic sensor working, everything else doesn't

--- backend/tessel/sensors.js
+++ backend/tessel/sensors.js
@@ -1,7 +1,7 @@
 var request = require("request");
-var http = require("http");
-var os = require("os");
-var path = require("path");
+// var http = require("http");
+// var os = require("os");
+// var path = require("path");
 
 var five = require("johnny-five");
 var Tessel = require("tessel-io");
@@ -9,33 +9,24 @@ var board = new five.Board({
   io: new Tessel()
 });
 
-var Express = require("express");
-var socket = require("socket.io");
+// var Express = require("express");
+// var SocketIO = require("socket.io");
 
-var app = new Express();
-var server = new http.Server(app);
-var io = new socket(server);
-io.on("connection", socket => {
-  //   clients.add(socket);
-  //   socket.on("disconnect", () => clients.delete(socket));
-  console.log("test!");
-});
+// var application = new Express();
+// var server = new http.Server(application);
+// var io = new SocketIO(server);
 
-app.use(Express.static(path.join(__dirname, "/app")));
-app.use("/vendor", Express.static(__dirname + "/node_modules/"));
+// application.use(Express.static(path.join(__dirname, "/app")));
+// application.use("/vendor", Express.static(__dirname + "/node_modules/"));
 
 board.on("ready", () => {
-  //Attempt of connection
-  //   var clients = new Set();
-  var updated = Date.now() - 5000;
-
-  var lcd = new five.LCD({
-    pins: ["a2", "a3", "a4", "a5", "a6", "a7"]
-  });
+  // var lcd = new five.LCD({
+  //   pins: ["a2", "a3", "a4", "a5", "a6", "a7"]
+  // });
 
   var monitor = new five.Multi({
     controller: "BME280",
-    freq: 15000
+    freq: 5000
   });
 
   const saveToDb = postData => {
@@ -44,11 +35,11 @@ board.on("ready", () => {
       body: JSON.stringify(postData),
       method: "POST",
       headers: {
-        "Content-Type": "app/json"
+        "Content-Type": "application/json"
       }
     };
     request(clientServerOptions, function(error, response) {
-      //   console.log(error, response);
+      console.log(error, response);
       return;
     });
   };
@@ -63,35 +54,65 @@ board.on("ready", () => {
     let recordData = {
       temperature: this.thermometer.fahrenheit,
       humidity: this.hygrometer.relativeHumidity,
-      hiveId: "5c3811a746b06c63be792b89",
-      created_at: Date.now
+      hiveId: "5c3811a746b06c63be792b89"
     };
-
-    saveToDb(recordData);
+    //   });
+    // });
+    // saveToDb(recordData);
   });
 
-  monitor.on("change", function() {
-    lcd.cursor(0, 0).print(`Temp: ${monitor.thermometer.fahrenheit}`);
-    lcd.cursor(1, 0).print(`Hum: ${monitor.hygrometer.relativeHumidity}`);
-    var now = Date.now();
+  // var clients = new Set();
+  // var updated = Date.now() - 5000;
 
-    if (now - updated >= 5000) {
-      updated = now;
-      clients.forEach(socket => {
-        socket.emit("report", {
-          thermometer: monitor.thermometer.fahrenheit,
-          hygrometer: monitor.hygrometer.relativeHumidity
-        });
-      });
-    }
-  });
+  //   io.on("connection", socket => {
+  //     clients.add(socket);
+  //     console.log("New client connected");
+  //     socket.on("disconnect", () => {
+  //       console.log("Client disconnected");
+  //     });
+  //   });
 
-  var port = 3000;
-  server.listen(port, () => {
-    console.log(`http://${os.networkInterfaces().wlan0[0].address}:${port}`);
-  });
+  // monitor.on("change", function() {
+  //   lcd.cursor(0, 0).print(`Temp: ${monitor.thermometer.fahrenheit}`);
+  //   lcd.cursor(1, 0).print(`Hum: ${monitor.hygrometer.relativeHumidity}`);
+  //   var now = Date.now();
 
-  process.on("SIGINT", () => {
-    server.close();
-  });
+  //   if (now - updated >= 5000) {
+  //     updated = now;
+  //     clients.forEach(socket => {
+  //       socket.emit("report", {
+  //         thermometer: monitor.thermometer.fahrenheit,
+  //         hygrometer: monitor.hygrometer.relativeHumidity
+  //       });
+  //     });
+  //   }
+  // });
+
+  //   io.on("connection", socket => {
+  // Allow up to 5 monitor sockets to
+  // connect to this enviro-monitor server
+  // if (clients.size < 5) {
+  //   clients.add(socket);
+  //   // When the socket disconnects, remove
+  //   // it from the recipient set.
+  // }
+
+  // socket.on("disconnect", () => clients.delete(socket));
+  //   });
+
+  // var port = 8000;
+  // server.listen(port, () => {
+  //   console.log(`http://${os.networkInterfaces().wlan0[0].address}:${port}`);
+  // });
+
+  // process.on("SIGINT", () => {
+  //   server.close();
+  // });
+
+  //   monitor.on("change", function() {
+  //     console.log("  Temperature  : ", this.thermometer.fahrenheit);
+  //     console.log("--------------------------------------");
+  //     console.log(" Humidity     : ", this.hygrometer.relativeHumidity);
+  //     console.log("--------------------------------------");
+  //   });
 });
